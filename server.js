12127ca8ac9b33552bae763df652dbfe2f32b9ca
KamiNaranjo <cami.tsuki@gmail.com>
2015-05-29 23:01:48 -0500

    Se organiza el contenido en una funcion

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ server.listen(4444);
 console.log("Server ON...");
 
 <!-- Método para convertir el arrayPolizasVigentes en un Json-->
-var jsonsPolizas = new Array();
-var contador = 0;
-while(arrayPolizasVigentes != null){
-	var polizaActual = {"idPoliza" : arrayPolizasVigentes[contador].idPoliza, "nombreProducto" : arrayPolizasVigentes[contador].nombreProducto, 
-	"fechaInicio" : arrayPolizasVigentes[contador].fechaInicio, "fechaFin" : arrayPolizasVigentes[contador].fechaFin, "valorAsegurado" : arrayPolizasVigentes[contador].valorAsegurado};
-	jsonsPolizas.push(polizaActual);
-}
+function polizasVigentesJson(){
+	var jsonsPolizas = new Array();
+	var contador = 0;
+	while(arrayPolizasVigentes != null){
+		var polizaActual = {"idPoliza" : arrayPolizasVigentes[contador].idPoliza, "nombreProducto" : arrayPolizasVigentes[contador].nombreProducto, 
+		"fechaInicio" : arrayPolizasVigentes[contador].fechaInicio, "fechaFin" : arrayPolizasVigentes[contador].fechaFin, "valorAsegurado" : arrayPolizasVigentes[contador].valorAsegurado};
+		jsonsPolizas.push(polizaActual);
+	}
+} 
+
 
