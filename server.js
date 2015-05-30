a3ef91e8f8d1e492f7e33b0efcaf5593428e08ee
KamiNaranjo <cami.tsuki@gmail.com>
2015-05-29 22:53:27 -0500

    Se adiciona el metodo que convierte el array de polizas en un objeto tipo JSON

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 function onRequest(request, response){
 	response.writeHead(200,{"content-type": "text/html"});
 	response.end("Generic response");
@@ -7,3 +8,13 @@ var http = require("http");
 var server = http.createServer(onRequest);
 server.listen(4444);
 console.log("Server ON...");
+
+<!-- Método para convertir el arrayPolizasVigentes en un Json-->
+var jsonsPolizas = new Array();
+var contador = 0;
+while(arrayPolizasVigentes != null){
+	var polizaActual = {"idPoliza" : arrayPolizasVigentes[contador].idPoliza, "nombreProducto" : arrayPolizasVigentes[contador].nombreProducto, 
+	"fechaInicio" : arrayPolizasVigentes[contador].fechaInicio, "fechaFin" : arrayPolizasVigentes[contador].fechaFin, "valorAsegurado" : arrayPolizasVigentes[contador].valorAsegurado};
+	jsonsPolizas.push(polizaActual);
+}
+
