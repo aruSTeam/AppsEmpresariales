30abbead88a29a3de3b9d502a380d1628eed3233
KamiNaranjo <cami.tsuki@gmail.com>
2015-05-31 17:14:43 -0500

    Se organiza un poco el texto y se pone a imprimir en consola para hacer seguimiento

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function onRequest(request, response){
 	//var res = recuperarPolizasVigentes(id);
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	var polizasJson = polizasVigentesJson(id);
-	console.log(polizasJson);
+	console.log("para la cedula " + id + "el server respondio: " + polizasJson);
 	response.end(polizasJson);
 }
 
@@ -34,15 +34,12 @@ function polizasVigentesJson(cedula){
 	var stringJsonPoliza = new Array;
 	var polizaActual = new Array;
 	var jsonsPolizas = new Array;
-	for(var contador = 0; contador < arrayPolizasVigentes.length; contador++){
-		if(arrayPolizasVigentes[contador].idUsuario == cedula){
-			polizaActual =  {"idPoliza" : arrayPolizasVigentes[contador].idPoliza, "nombreProducto" : arrayPolizasVigentes[contador].nombreProducto,			
-			"fechaInicio" : arrayPolizasVigentes[contador].fechaInicio , "fechaFin":arrayPolizasVigentes[contador].fechaFin ,
-			"valorAsegurado" : arrayPolizasVigentes[contador].valorAsegurado}; 
-			stringJsonPoliza = JSON.stringify(polizaActual);
-			jsonsPolizas = jsonsPolizas + stringJsonPoliza;
-			//jsonsPolizas.push(stringJsonPoliza);
-		}	
+	for(var contador = 0; contador < arrayPolizasVigentes.length; contador++){		
+		polizaActual =  {"idPoliza" : arrayPolizasVigentes[contador].idPoliza, "nombreProducto" : arrayPolizasVigentes[contador].nombreProducto,
+				"fechaInicio" : arrayPolizasVigentes[contador].fechaInicio , "fechaFin":arrayPolizasVigentes[contador].fechaFin ,
+				"valorAsegurado" : arrayPolizasVigentes[contador].valorAsegurado}; 
+		stringJsonPoliza = JSON.stringify(polizaActual);
+		jsonsPolizas = jsonsPolizas + stringJsonPoliza;
 	}
 	return jsonsPolizas;
 }
@@ -54,6 +51,7 @@ var arrayPolizasVigentes = new Array();
 
 //Creando un objeto usuario y agregando valores
 var usuario = new Array();
+
 usuario[0] = new Object();
 usuario[0].idUsuario = "0001";
 
@@ -62,12 +60,11 @@ usuario[1].idUsuario = "0002";
 
 //Creando un objeto poliza y agregando valores
 var poliza = new Array();
+
 poliza[0] = new Object();
 poliza[0].idPoliza = "1122334455";
 poliza[0].idUsuario = "0001";
 poliza[0].nombreProducto = "Vida";
-//poliza[0].fechaInicio = DATE(2014-01-01);
-//poliza[0].fechaFin = DATE(2015-09-23);
 poliza[0].fechaInicio = new Date(2014,1-1,01);    
 poliza[0].fechaFin = new Date(2015,2-1,23); 
 poliza[0].valorAsegurado = 1000000000;
@@ -76,8 +73,6 @@ poliza[1] = new Object();
 poliza[1].idPoliza = "2233445566";
 poliza[1].idUsuario = "0002";
 poliza[1].nombreProducto = "Hogar";
-//poliza[1].fechaInicio = DATE(2015-01-01);
-//poliza[1].fechaFin = DATE(2016-01-01);
 poliza[1].fechaInicio = new Date(2015,1-1,01);
 poliza[1].fechaFin = new Date(2016,1-1,01);
 poliza[1].valorAsegurado = 45600000;
@@ -86,8 +81,6 @@ poliza[2] = new Object();
 poliza[2].idPoliza = "5566778899";
 poliza[2].idUsuario = "0001";
 poliza[2].nombreProducto = "Responsabilidad Civil";
-//poliza[2].fechaInicio = DATE(2012-05-23);
-//poliza[2].fechaFin = DATE(2019-05-23);
 poliza[2].fechaInicio = new Date(2012,5-1,23);
 poliza[2].fechaFin = new Date(2019,5-1,23);
 poliza[2].valorAsegurado = 2050740000;
@@ -96,8 +89,6 @@ poliza[3] = new Object();
 poliza[3].idPoliza = "0044556677";
 poliza[3].idUsuario = "0001";
 poliza[3].nombreProducto = "Autos";
-//poliza[3].fechaInicio = DATE(2012-05-23);
-//poliza[3].fechaFin = DATE(2019-05-23);
 poliza[3].fechaInicio = new Date(2012,5-1,23);
 poliza[3].fechaFin = new Date(2015,4-1,23);
 poliza[3].valorAsegurado = 2050740000;
@@ -106,8 +97,6 @@ poliza[4] = new Object();
 poliza[4].idPoliza = "1122334455";
 poliza[4].idUsuario = "0001";
 poliza[4].nombreProducto = "Vida";
-//poliza[4].fechaInicio = DATE(2015-01-01);
-//poliza[4].fechaFin = DATE(2016-01-01);
 poliza[4].fechaInicio = new Date(2015,1-1,01);
 poliza[4].fechaFin = new Date(2016,1-1,01);
 poliza[4].valorAsegurado = 2056700000;
@@ -116,8 +105,6 @@ poliza[5] = new Object();
 poliza[5].idPoliza = "5566778899";
 poliza[5].idUsuario = "0002";
 poliza[5].nombreProducto = "Responsabilidad Civil";
-//poliza[5].fechaInicio = DATE(2015-13-03);
-//poliza[5].fechaFin = DATE(2016-07-11);
 poliza[5].fechaInicio = new Date(2015,3-1,13);
 poliza[5].fechaFin = new Date(2016,7-1,11);
 poliza[5].valorAsegurado = 3243000000;
@@ -125,11 +112,3 @@ poliza[5].valorAsegurado = 3243000000;
 
 server.listen(4444);
 console.log("Server ON...");
-
-
-
-
-
- 
-
-
